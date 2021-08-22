2c26a2d7153663ce00498ce8f33ef0bd615fb739
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2021-08-22 11:10:56 -0700

    feat: Input word and look for it

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,9 +1,48 @@
 <template>
-  <h1>Hello World!</h1>
+  <div class="container">
+    <h5>Buscar palabra:</h5>
+    <b-form-input
+      v-model="word"
+      placeholder="Ingrese una palabra"
+      id="searchInput"
+    ></b-form-input>
+    <button @click="fetchData">Buscar</button>
+    <p>{{ word }}</p>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return { word: undefined };
+  },
+
+  methods: {
+    get_image: function() {},
+    fetchData: function() {
+      this.$http
+        .get("https://serpapi.com/playground?q=Apple&tbm=isch&ijn=0")
+        .then(response => {
+          const data = response.json();
+          console.log(data);
+        });
+    }
+  }
+};
 </script>
 
-<style></style>
+<style>
+#searchInput {
+  width: 50%;
+  display: flex;
+  justify-content: center;
+}
+
+.header {
+  width: 100%;
+}
+
+.container {
+  margin: 5%;
+}
+</style>
