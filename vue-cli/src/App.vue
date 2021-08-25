88472dc426a3abe2c63ec8bc9005c6b07f995a8e
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2021-08-25 06:14:59 -0700

    feat: Improve appearance

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="container">
+    <b-row class="mb-3" style="text-align:center;">
+      <h1>Imágenes del mundo</h1>
+    </b-row>
     <b-row>
-      <b-col class="searchInput">
-        <b-row>
+      <b-col>
+        <b-row class="searchInput">
           <b-col
             ><h5>Buscar palabra:</h5>
             <b-form-input
@@ -10,34 +13,56 @@
               placeholder="Ingrese una palabra"
               id="searchInput"
             ></b-form-input>
-            <b-button class="btn" variant="primary" @click="fetchData"
+            <b-button class="btn" variant="primary" @click="fetchUnsplashData"
               >Buscar</b-button
             ></b-col
           ></b-row
-        ><b-row v-if="bill_created">
-          <b-card-group deck>
-            <b-card
-              v-for="img in images"
+        ><b-row class="mt-3" v-if="bill_created">
+          <b-row>
+            <b-col
+              cols="5"
+              v-for="img in images.slice(
+                this.first_element,
+                this.first_element + 5
+              )"
               :key="img.id"
-              @click="update_points(img.id.toString())"
-              v-bind:title="
-                'Vendedor: ' + sellers.find(t => t['id'] === img.id)['name']
-              "
-              v-bind:img-src="img.url"
-              img-alt="Image"
-              img-top
-              tag="article"
-              style="max-width: 15rem;"
-              class="mb-2"
-            ></b-card>
-          </b-card-group>
+            >
+              <b-card
+                border-variant="secondary"
+                @click="update_points(img.id)"
+                v-bind:title="
+                  'Vendedor: ' +
+                    sellers.find(t => t['id'] === (img.id % 5) + 1)['name']
+                "
+                v-bind:img-src="img.url"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 15rem;"
+                class="mb-2 cardImage"
+              ></b-card
+            ></b-col>
+          </b-row>
         </b-row>
-        <b-row v-else
-          ><p>Nombre del vendedor ganador: {{ this.bill["seller"]["name"] }}</p>
-          <p>Producto: {{ this.bill["items"][0]["name"] }}</p>
-          <p>Cantidad vendida: {{ this.bill["items"][0]["price"] }}</p></b-row
+        <b-row v-else>
+          <b-card class="mt-3 bill" title="Factura"
+            ><b-list-group flush>
+              <b-list-group-item
+                >Nombre del vendedor ganador:
+                {{ this.bill["seller"]["name"] }}</b-list-group-item
+              >
+              <b-list-group-item
+                >Producto:
+                {{ this.bill["items"][0]["name"] }}</b-list-group-item
+              >
+              <b-list-group-item
+                >Cantidad vendida:
+                {{ this.bill["items"][0]["quantity"] }}</b-list-group-item
+              >
+            </b-list-group></b-card
+          ></b-row
         ></b-col
-      ><b-col
+      ><b-col cols="3"
         ><b-card
           class="mt-3"
           bg-variant="light"
@@ -46,7 +71,7 @@
           :title="seller.name"
           ><b-row
             ><b-col>Puntos acumulados: {{ sellers_points[seller.id] }}</b-col
-            ><b-col
+            ><b-col v-if="sellers_points[seller.id] < 20"
               >Puntos faltantes: {{ 20 - sellers_points[seller.id] }}</b-col
             ></b-row
           >
@@ -65,7 +90,7 @@ export default {
       sellers_id: [1, 2, 3, 4, 5],
       sellers: [],
       images: [],
-      first_element: 1,
+      first_element: 0,
       next_images: [],
       sellers_points: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 }
     };
@@ -75,7 +100,7 @@ export default {
       this.$http
         .get(`https://api.alegra.com/api/v1/sellers/${id}`, {
           headers: {
-            Authorization: `Basic ${process.env.VUE_APP_CREDENTIALS}`
+            Authorization: `Basic YW5vcGxhNEBnbWFpbC5jb206MWY4NmNjODMxNTc5ZGU0OGRiMWQ=`
           }
         })
         .then(response => {
@@ -87,28 +112,26 @@ export default {
 
   methods: {
     update_points: function(id) {
-      this.sellers_points[id] += 3;
-      if (this.next_images.length !== 0) {
-        this.images = [...next_images];
-        this.next_images = [];
-      } else {
-        this.first_element += 10;
-        this.fetchData();
-      }
-      if (this.sellers_points[id] >= 20) {
+      const n_id = (id % 5) + 1;
+      this.sellers_points[n_id.toString()] += 3;
+      this.first_element += 5;
+      if (this.sellers_points[n_id.toString()] >= 20) {
         this.bill_created = false;
-        const sum = this.sellers.reduce((a, b) => a + b, 0);
+        const sum = Object.values(this.sellers_points).reduce(
+          (a, b) => a + b,
+          0
+        );
         const date = new Date();
         const date_string = `${date.getFullYear()}-${date.getMonth() +
           1}-${date.getDate()}`;
         const bill = {
           date: date_string,
           dueDate: date_string,
-          client: process.env.VUE_APP_CLIENT,
+          client: 1,
           seller: id,
           items: [
             {
-              id: process.env.VUE_APP_PRODUCT,
+              id: 1,
               price: 3,
               quantity: parseFloat(sum)
             }
@@ -117,7 +140,7 @@ export default {
         this.$http
           .post("https://api.alegra.com/api/v1/invoices", bill, {
             headers: {
-              Authorization: `Basic ${process.env.VUE_APP_CREDENTIALS}`
+              Authorization: `Basic YW5vcGxhNEBnbWFpbC5jb206MWY4NmNjODMxNTc5ZGU0OGRiMWQ=`
             }
           })
           .then(response => {
@@ -126,55 +149,35 @@ export default {
           .then(data => (this.bill = data));
       }
     },
-    fetchData: function() {
-      gapi.client.setApiKey("AIzaSyDFanaXHAyTj9KPXAlH5VpBRqEUB7PZTQk");
-      return gapi.client
-        .load(
-          "https://content.googleapis.com/discovery/v1/apis/customsearch/v1/rest"
-        )
-        .then(
-          () => {
-            console.log("GAPI client loaded for API");
-          },
-          err => {
-            console.error("Error loading GAPI client for API", err);
-          }
-        )
-        .then(() => {
-          return gapi.client.search.cse
-            .list({
-              cx: "6ce5fdff9abf5811f",
-              q: this.word,
-              searchType: "image",
-              start: this.first_element
-            })
-            .then(
-              function(response) {
-                return response["result"]["items"];
-              },
-              function(err) {
-                console.error("Execute error", err);
-              }
-            );
-        })
-        .then(img => {
-          this.images = img.slice(0, 5).map((item, index) => {
-            return { id: index + 1, url: item["link"] };
-          });
-          this.next_images = img.slice(5, 10).map((item, index) => {
-            return { id: index + 1, url: item["link"] };
-          });
-        });
+    fetchUnsplashData: function() {
+      for (let index = 1; index < 11; index++) {
+        var base_url = `https://api.unsplash.com/search/photos?query=${this.word}?page=${index}`;
+        this.$http
+          .get(base_url, {
+            headers: {
+              Authorization: `Client-ID F-W4E8ZVBHBuwHCV87bwAHON8kyMQZlkXsXimEQdTBE`
+            }
+          })
+          .then(response => {
+            return response.json();
+          })
+          .then(
+            data =>
+              (this.images = this.images.concat(
+                data["results"].map((t, index) => {
+                  return { id: index + 1, url: t["urls"]["regular"] };
+                })
+              ))
+          );
+      }
     }
   }
 };
 </script>
 
 <style>
-searchInput {
+.searchInput {
   width: 50%;
-  display: flex;
-  justify-content: center;
 }
 
 .header {
@@ -195,5 +198,13 @@ searchInput {
   width: 50px;
   height: 50px;
   margin: 2%;
+}
+
+.bill {
+  width: 60%;
+}
+
+.cardImage:hover {
+  box-shadow: 20px 20px 40px 0px rgba(0, 0, 0, 0.5);
 }
 </style>
